4eba2c9e9629769c2c0edff3da647d8a46131c02
kumarisneha <kumarisneha102@gmail.com>
2020-07-21 23:37:35 +0530

    Did some changes in models file

--- addrbookproj/addrbookapp/models.py
+++ addrbookproj/addrbookapp/models.py
@@ -9,4 +9,7 @@ class Address(models.Model):
     user = models.CharField(max_length = 30)
     address = models.CharField(max_length = 100) 
     email_id = models.EmailField(max_length = 50, unique=True)
-    phone_number = models.CharField(_('number'), max_length=10 ,unique=True)  
+    phone_number = models.CharField(_('number'), max_length=10 ,unique=True)
+
+    def __str__(self):
+        return self.user
